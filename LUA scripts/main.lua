6f9a1ec20491231b6538a3d51988f00a3435bfe5
pjotr.brunain <pjotr.brunain@student.odisee.be>
2024-04-15 23:05:02 +0200

    Implemented JSON commands to LUA

--- LUA scripts/main.lua
+++ LUA scripts/main.lua
@@ -1,6 +1,7 @@
 -- Instanciate both helper classes to help with jsonWriting and monitorWriting
 local jsonHelper = require("JsonFileHelper")
 local monitorWriter = require("monitorWriter")
+local pretty = require "cc.pretty"
 
 -- Instanciate and initialize the peripheral wrapper
 local peripherals = require("wrapPeripherals")
@@ -11,17 +12,27 @@ monitorWriter.Init(peripherals.GetMonitor())
 local colonyExtractor = require("extractTasks")
 local aeInterface = require("aeInterface")
 
--- Extract colonyData and write it to requests.json
-local colonyData = colonyExtractor.ExtractTasks(peripherals, monitorWriter)
-jsonHelper.WriteJson("requests.json", colonyData)
+-- Infinite loop
+while true do
+    -- Extract colonyData and write it to requests.json
+    local colonyData = colonyExtractor.ExtractTasks(peripherals, monitorWriter)
+    jsonHelper.WriteJson("requests.json", colonyData)
 
--- Extract ae data and write it to aeData.json
-local aeItems = aeInterface.ExtractItems(peripherals, monitorWriter)
-local aeCraftable = aeInterface.ExtractPatterns(peripherals, monitorWriter)
-local aeData = {}
-aeData["items"] = aeItems
-aeData["patterns"] = aeCraftable
-aeData["colony"] = peripherals.GetColonyIntegrator().getColonyName()
-local colonyAeData = {}
-table.insert(colonyAeData, aeData)
-jsonHelper.WriteJson("aeData.json", colonyAeData)
+    -- Extract ae data and write it to aeData.json
+    local aeItems = aeInterface.ExtractItems(peripherals, monitorWriter)
+    local aeCraftable = aeInterface.ExtractPatterns(peripherals, monitorWriter)
+    local aeData = {}
+    aeData["items"] = aeItems
+    aeData["patterns"] = aeCraftable
+    aeData["colony"] = peripherals.GetColonyIntegrator().getColonyName()
+    local colonyAeData = {}
+    table.insert(colonyAeData, aeData)
+    jsonHelper.WriteJson("aeData.json", colonyAeData)
+
+    -- Read in the commands and execute them
+    local commands = jsonHelper.ReadJson("commands.json")
+    aeInterface.ProcessCommands(peripherals, monitorWriter, commands)
+
+    -- Sleep for the given sleeptime
+    os.sleep(peripherals.GetSleepTime())
+end
--- LUA scripts/wrapPeripherals.lua
+++ LUA scripts/wrapPeripherals.lua
@@ -2,9 +2,15 @@
 local Monitor = nil
 local ColonyIntegrator = nil
 local PlayerSideMe = nil
+local PlayerSideMeInventory = nil
+local ColonySideMeInventory = nil
 local ColonySideMe = nil
 
 local MonitorWriter = nil
+local SleepTime = 5
+
+local JsonFileHelper = require("JsonFileHelper")
+local pretty = require "cc.pretty"
 
 -- Decision table with functions for each different kind of peripheral
 local peripheralTable = 
@@ -19,7 +25,7 @@ local peripheralTable =
     end,
     -- Asks which me bridge on which side is either colony side or player side
     ["meBridge"] = function (peripheral, side)
-        print("Is the MEBridge on the " .. side .. " side connected to the player system or the colony system? (player/colony)")
+        print("Is the MEBridge with the name " .. side .. " connected to the player system or the colony system? (player/colony)")
         local answer = nil
         answer = io.read()
         while not (answer == "player" or answer == "colony") do
@@ -31,47 +37,126 @@ local peripheralTable =
         elseif answer == "colony" then
             ColonySideMe = peripheral
         end
+    end,
+    ["ae2:interface"] = function (peripheral, name)
+        print("Is the interface with the name ".. name .. " connected to the player system or the colony system? (player/colony)")
+        local answer = nil
+        answer = io.read()
+        while not (answer == "player" or answer == "colony") do
+            print("Please answer with 'player' or 'colony' (player/colony)")
+            answer = io.read()
+        end
+        if answer == "player" then
+            PlayerSideMeInventory = peripheral
+        elseif answer == "colony" then
+            ColonySideMeInventory = peripheral
+        end
     end
 }
 
+-- Splits a string based on a given seperator sep
+local function split (inputstr, sep)
+    if sep == nil then
+            sep = "%s"
+    end
+    local t={}
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+            table.insert(t, str)
+    end
+    return t
+end
+
+-- Function to map the different peripherals
+local function MapPeripherals()
+    -- Get all names of connected peripherals and loop over them
+    local peripherals = peripheral.getNames()
+        for i, peripheralName in ipairs(peripherals) do
+            local func = nil
+            -- Wrap the peripheral
+            local wrappedPeripheral = peripheral.wrap(peripheralName)
+            if not (wrappedPeripheral == nil) then 
+                -- Get the type of the peripheral
+                local type = peripheral.getType(wrappedPeripheral)
+                local splitTypes = split(type, "%s")
+                -- pretty.pretty_print(splitTypes)
+
+                -- If the split types has more then 1 value take the second value (THIS IS HARDCODED)
+                local i = 1
+                if table.getn(splitTypes) > 1 then
+                    i = 2
+                end
+
+                -- Get the function for the given type from the functiontable
+                func = peripheralTable[splitTypes[i]]
+                --print(peripheral.getType(wrappedPeripheral))
+            end
+            -- If the peripheral type has a funtion, run that function to put the wrapped peripheral in the correct variable
+            if not (func == nil) then func(wrappedPeripheral, peripheralName) end
+        end
+
+        -- Check if the colonyIntegrator is in a colony, if not exit the program
+        if not ColonyIntegrator.isInColony() then 
+            MonitorWriter.WriteLine("Block is not in a colony", Monitor)
+            os.exit()
+        end
+
+        -- Ask the user for the sleeptime
+        print("Please insert the checkInterval in seconds (default 5 seconds)")
+        local answer = io.read()
+        local num = tonumber(answer)
+        if num ~= nil then SleepTime = num end
+end
+
+-- Function to make the object to write to the savedState file so the user doesn't have to keep inputting which peripheral is which
+local function WriteConfigFile()
+    local peripherals = {}
+    if Monitor ~= nil then peripherals["Monitor"] = peripheral.getName(Monitor) end
+    if ColonyIntegrator ~= nil then peripherals["ColonyIntegrator"] = peripheral.getName(ColonyIntegrator) end
+    if PlayerSideMe ~= nil then peripherals["PlayerSideMe"] = peripheral.getName(PlayerSideMe) end
+    if PlayerSideMeInventory ~= nil then peripherals["PlayerSideMeInventory"] = peripheral.getName(PlayerSideMeInventory) end
+    if ColonySideMeInventory ~= nil then peripherals["ColonySideMeInventory"] = peripheral.getName(ColonySideMeInventory) end
+    if ColonySideMe ~= nil then peripherals["ColonySideMe"] = peripheral.getName(ColonySideMe) end
+    if SleepTime ~= nil then peripherals["SleepTime"] = SleepTime end
+
+    JsonFileHelper.WriteJson("savedState.json", peripherals)
+end
+
 -- Initialize everything to detect the different peripherals
 local function Initialize(monitorWriter)
+    -- Cache MonitorWriter
     MonitorWriter = monitorWriter
 
-    -- wrap all peripherals on each side
-    local topPeripheral = peripheral.wrap("top")
-    local bottomPeripheral = peripheral.wrap("bottom")
-    local leftPeripheral = peripheral.wrap("left")
-    local rightPeripheral = peripheral.wrap("right")
-    local frontPeripheral = peripheral.wrap("front")
-    local backPeripheral = peripheral.wrap("back")
+    -- Check if a savedState exists
+    if JsonFileHelper.file_exists("savedState.json") then
 
-    -- Check each peripheral against the peripheralTable
-    local func = nil
-    if not (topPeripheral == nil) then func = peripheralTable[peripheral.getType(topPeripheral)] end
-    if not (func == nil) then func(topPeripheral, "top") end
-    func = nil
-    if not (bottomPeripheral == nil) then func = peripheralTable[peripheral.getType(bottomPeripheral)] end
-    if not (func == nil) then func(bottomPeripheral, "bottom") end
-    func = nil
-    if not (leftPeripheral == nil) then func = peripheralTable[peripheral.getType(leftPeripheral)] end
-    if not (func == nil) then func(leftPeripheral, "left") end
-    func = nil
-    if not (rightPeripheral == nil) then func = peripheralTable[peripheral.getType(rightPeripheral)] end
-    if not (func == nil) then func(rightPeripheral, "right") end
-    func = nil
-    if not (frontPeripheral == nil) then func = peripheralTable[peripheral.getType(frontPeripheral)] end
-    if not (func == nil) then func(frontPeripheral, "front") end
-    func = nil
-    if not (backPeripheral == nil) then func = peripheralTable[peripheral.getType(backPeripheral)] end
-    if not (func == nil) then func(backPeripheral, "back") end
-    func = nil
+        -- Ask the user if they want to use the savedState
+        print("previous config file found. Use this? (yes/no)")
+        local answer = io.read()
+        while not (answer == "yes" or answer == "no") do
+            print("Please answer with 'yes' or 'no' (yes/no)")
+            answer = io.read()
+        end
 
-    -- Check if the colonyIntegrator is in a colony, if not exit the program
-    if not ColonyIntegrator.isInColony() then 
-        MonitorWriter.WriteLine("Block is not in a colony", Monitor)
-        os.exit()
+        -- If the answer is yes then map the peripherals according to the savedState
+        if answer == "yes" then
+            local peripherals = JsonFileHelper.ReadJson("savedState.json")
+            if peripherals.Monitor ~= nil then Monitor = peripheral.wrap(peripherals.Monitor) end
+            if peripherals.ColonyIntegrator ~= nil then ColonyIntegrator = peripheral.wrap(peripherals.ColonyIntegrator) end
+            if peripherals.PlayerSideMe ~= nil then PlayerSideMe = peripheral.wrap(peripherals.PlayerSideMe) end
+            if peripherals.PlayerSideMeInventory ~= nil then PlayerSideMeInventory = peripheral.wrap(peripherals.PlayerSideMeInventory) end
+            if peripherals.ColonySideMeInventory ~= nil then ColonySideMeInventory = peripheral.wrap(peripherals.ColonySideMeInventory) end
+            if peripherals.ColonySideMe ~= nil then ColonySideMe = peripheral.wrap(peripherals.ColonySideMe) end
+            if peripherals.SleepTime ~= nil then SleepTime = peripherals.SleepTime end
+        elseif answer == "no" then
+            -- If the answer is no then map the peripherals like new
+            MapPeripherals()
+        end
+    else
+        -- If no savedState exists, map the peripherals like new
+        MapPeripherals()
     end
+    -- Write the config file away
+    WriteConfigFile()
 end
 
 -- This is a simple test function to test if it finds all peripherals
@@ -103,12 +188,30 @@ local function GetColonyMeBridge()
     return ColonySideMe
 end
 
+-- Returns the playerside interface connected to the playerside Me system as object
+local function GetPlayerMeBridgeInventory()
+    return PlayerSideMeInventory
+end
+
+-- Returns the colonyside interface connected to the colonyside Me system as object
+local function GetColonyMeBridgeInventory()
+    return ColonySideMeInventory
+end
+
+-- Returns the SleepTime configured
+local function GetSleepTime()
+    return SleepTime
+end
+
 -- This returns the public functions so other scripts can use them
 return {Initialize = Initialize,
 GetMonitor = GetMonitor, 
 GetColonyIntegrator = GetColonyIntegrator,
 GetPlayerMeBridge = GetPlayerMeBridge,
 GetColonyMeBridge = GetColonyMeBridge,
-TestFunction = TestFunction}
+TestFunction = TestFunction,
+GetPlayerMeBridgeInventory = GetPlayerMeBridgeInventory,
+GetColonyMeBridgeInventory = GetColonyMeBridgeInventory,
+GetSleepTime = GetSleepTime}
 
 --TestFunction()
