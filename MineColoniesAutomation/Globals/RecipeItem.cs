c9ef060a7da2a2d51c595ebbd9bb9192316cddf6
pjotr.brunain <pjotr.brunain@student.odisee.be>
2024-05-24 23:10:23 +0200

    Reworked Item extraction to make it better for json serialization and made it more robust

---
+++ MineColoniesAutomation/Globals/RecipeItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Globals
+{
+    public class RecipeItem : IEquatable<RecipeItem>
+    {
+        public List<string> Items { get; set; } = new List<string>();
+        public int Amount {  get; set; }
+
+        public bool Equals(RecipeItem? other)
+        {
+            if (other == null) return false;
+            return Items.Equals(other.Items) && Amount.Equals(other.Amount);
+        }
+    }
+}
